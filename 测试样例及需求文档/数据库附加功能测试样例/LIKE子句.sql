9f11310860d0b76e86d119cc6e09261a8be3cd64
HaoyunHong <honghaoyun1999@163.com>
2019-06-18 07:39:48 +0800

    Update LIKE子句.sql
    测例支持LIKE子句的所有格式，可以选择列输出，支持单引号或者双引号

--- 测试样例及需求文档/数据库附加功能测试样例/LIKE子句.sql
+++ 测试样例及需求文档/数据库附加功能测试样例/LIKE子句.sql
@@ -1,12 +1,20 @@
 CREATE DATABASE CAFE;
 USE CAFE;
 CREATE TABLE menu(price INT, cost INT, kind CHAR, PRIMARY KEY(kind));
-INSERT INTO menu(price, cost, kind) VALUES (15, 5, "milk_coffee");
-INSERT INTO menu(price, cost, kind) VALUES (20, 8, "milk_cake");
-INSERT INTO menu(price, cost, kind) VALUES (30, 12, "honey_waffle");
-INSERT INTO menu(price, cost, kind) VALUES (15, 5, "fruit_cake");
-INSERT INTO menu(price, cost, kind) VALUES (22, 10, "ice_cream_cake");
-INSERT INTO menu(price, cost, kind) VALUES (20, 8, "honey_cake");
+INSERT INTO menu(price, cost, kind) VALUES (15, 5, "milk coffee");
+INSERT INTO menu(price, cost, kind) VALUES (20, 8, "milk cake");
+INSERT INTO menu(price, cost, kind) VALUES (30, 12, "abc");
+INSERT INTO menu(price, cost, kind) VALUES (15, 5, "red rose cake");
+INSERT INTO menu(price, cost, kind) VALUES (22, 10, "pink rose cake");
+INSERT INTO menu(price, cost, kind) VALUES (20, 8, "ki");
 SELECT * from menu WHERE kind LIKE '%cake';
 SELECT price from menu WHERE kind LIKE '%cake';
+SELECT * from menu WHERE kind LIKE "milk%";
+SELECT price from menu WHERE kind LIKE "milk%";
+SELECT * from menu WHERE kind LIKE '%rose%';
+SELECT price from menu WHERE kind LIKE '%rose%';
+SELECT * from menu WHERE kind LIKE '_i';
+SELECT price from menu WHERE kind LIKE 'k_';
 DROP DATABASE CAFE;
+ /*可以选择列输出*/
+ /*支持单引号或者双引号*/
