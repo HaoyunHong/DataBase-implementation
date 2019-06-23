1ddfb3aa24e29ed5979c08a0173a3f4f955dedf7
HaoyunHong <honghaoyun1999@163.com>
2019-06-23 14:14:27 +0800

    对corner case的处理进行字符串对比

--- 测试样例及需求文档/数据库附加功能测试样例/SQL字符串函数.sql
+++ 测试样例及需求文档/数据库附加功能测试样例/SQL字符串函数.sql
@@ -1,4 +1,8 @@
 SELECT CHAR_LENGTH("RUNOOB") AS LengthOfString;
+SELECT CHAR_LENGTH("RUNOOB")AS LengthOfString;
 SELECT CHARACTER_LENGTH("RUNOOB") AS LengthOfString;
+SELECT CHARACTER_LENGTH("RUNOOB")AS LengthOfString;
 SELECT CONCAT("SQL ", "Runoob ", "Gooogle ", "Facebook") AS ConcatenatedString;
+SELECT CONCAT("SQL ", "Runoob ", "Gooogle ", "Facebook")AS ConcatenatedString;
+SELECT CONCAT_WS("-", "SQL", "Tutorial", "is", "fun!") AS ConcatenatedString;
 SELECT CONCAT_WS("-", "SQL", "Tutorial", "is", "fun!")AS ConcatenatedString;
